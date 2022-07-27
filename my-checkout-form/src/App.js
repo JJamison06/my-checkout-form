57e27fec4d2c2be7555cd186f97f78c469b44a50
JJamison06 <jamil.jamison@cdpipelinedevshop.com>
2022-07-27 17:00:28 +0000

    first commit

---
+++ my-checkout-form/src/App.js
@@ -0,0 +1,13 @@
+import Checkout from './components/Checkout';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Checkout></Checkout>
+      
+    </div>
+  );
+}
+
+export default App;
